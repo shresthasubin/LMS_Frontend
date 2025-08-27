abb8be01c85c51c6489f77bac3b8b50ae4a2ebd3
shresthasubin <shresthasubin938@gmail.com>
2025-08-27 19:53:34 +0545

    frontend

--- src/utils/config.js
+++ src/utils/config.js
@@ -12,10 +12,10 @@ export const privateAPI = axios.create({
     withCredentials: true
 })
 
-// privateAPI.interceptors.request.use((config) => {
-//     const token = Cookies.get('token');
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-// })
+privateAPI.interceptors.request.use((config) => {
+    const token = Cookies.get('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
